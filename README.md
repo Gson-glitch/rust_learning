d29d72e3a3790b252dd178e64a3b0ea91b2ef09f
Gloryson <glorysonhorace@gmail.com>
2026-01-09 13:44:53 +0300

    data structures: structs

--- README.md
+++ README.md
@@ -34,7 +34,7 @@ A personal repository dedicated to documenting my progress through the **[Rust C
 
 ### Phase 3: Data Structures & Organization
 
-- [ ] **Structs:** Defining data and implementing Methods (Videos 9-11)
+- [X] **Structs:** Defining data and implementing Methods (Videos 9-11)
 - [ ] **Enums:** Mastering `Enum`, `Match`, and `if let` (Videos 12-14)
 - [ ] **Modules:** Packages, Crates, and Visibility (Videos 15-18)
 
---
+++ src/bin/struct.rs
@@ -0,0 +1,80 @@
+struct Rectangle {
+    width: f32,
+    height: f32,
+}
+
+// Fields (Attributes) in Rust
+struct Player {
+    name: String, // field
+    stats: f32,
+}
+
+// In Rust, impl (implementation) is the keyword used to define the behavior for a specific type.
+// Think of it this way, if struct is a blueprint for data, then impl is the actual definition
+// incorporating methods, attributes, etc. all which belong to that specific struct
+impl Player {
+    // Associated Functions
+    // This are equivalents to @classmethods in Python.
+    // Usually used for constructors (like new).
+    fn new(name: String, stats: f32) -> Self {
+        Self { name, stats }
+    }
+
+    // Methods (instance methods, must have self as the first parameter)
+    fn get_stats(&self) -> f32 {
+        self.stats
+    }
+
+    fn print_player(&self) {
+        println!(
+            "Player Name: {}\nPlayer Stats: {}\n",
+            self.name,
+            self.get_stats()
+        );
+    }
+}
+
+fn main() {
+    #[derive(Debug)]
+    struct Point {
+        x: i8,
+        y: i8,
+    }
+
+    #[derive(Debug)]
+    struct Color {
+        r: f32,
+        g: f32,
+        b: f32,
+    }
+
+    let coord: Point = Point { x: 4, y: 8 };
+    let red: Color = Color {
+        r: 255.0,
+        g: 0.0,
+        b: 0.0,
+    };
+
+    println!("Coordinates: {:?}\nx: {}, y: {}", coord, coord.x, coord.y);
+    println!(
+        "Color Red: {:?}\nr: {}, g: {}, b: {}",
+        red, red.r, red.g, red.b
+    );
+
+    // Area of Rectangle
+    let rec1: Rectangle = Rectangle {
+        width: 20.0,
+        height: 40.0,
+    };
+
+    println!("The area of 'rec1' = {}", calc_area(&rec1));
+    println!("'rec1' dimensions: {}, {}", rec1.width, rec1.height);
+
+    // Player Stats
+    let player1: Player = Player::new(String::from("John Doe"), 90.4);
+    player1.print_player();
+}
+
+fn calc_area(rec: &Rectangle) -> f32 {
+    rec.width * rec.height
+}
